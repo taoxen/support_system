f109d7a2a47b5fc2f5d82d2db39d04fc884fbf8e
Ravindra Gokhale <ravigokhale@gmail.com>
2024-01-24 13:41:25 +0530

    Initial commit for the Support system portal. This portal is being developed for Rare systems to manage their support calls.

---
+++ support_system/support_system/doctype/support_calls/support_calls_list.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2024, Ravi Gokhale and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings['Support Calls'] = {
+    refresh: function(listview) {
+        console.log("Triggered")
+        // console.log(listview.fields)
+        // $("status").hide();
+	    // console.log($("_liked_by"))
+        // $("_liked_by").hide();
+	    // $(".frappe-timestamp").hide();
+	    // $(".avatar-small").hide();
+	    // $(".comment-count").hide();
+	},
+
+}
---
+++ support_system/support_system/doctype/support_calls/test_support_calls.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ravi Gokhale and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSupportCalls(FrappeTestCase):
+	pass
